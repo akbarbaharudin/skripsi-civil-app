2c6553109241ec82f0cb6c4468278748235ca909
akbarbaharudin <akbarbaharudin102@gmail.com>
2026-02-05 05:14:31 +0700

    Add files via upload

--- app.py
+++ app.py
@@ -40,7 +40,7 @@ if st.button("HITUNG ESTIMASI", type="primary"):
     
     # === [ FAKTOR KALIBRASI ] ===
     # Target: ~37 Miliar.
-    FAKTOR_KOREKSI = 0.370 
+    FAKTOR_KOREKSI = 0.375 
     
     # 1. Estimasi Volume Beton (m³)
     tebal_ekuivalen = 0.35 + ((d - 6000) / 20000) 
